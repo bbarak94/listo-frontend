425cbe24217b61f8894568bcb4f0027e65bde81d
Itai Rotstein <itairotstein@gmail.com>
2022-05-25 16:23:10 +0300

    solved login useLocation issue

--- src/pages/login-signup.jsx
+++ src/pages/login-signup.jsx
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-// import { useState, useEffect } from 'react'
-// // import { userService } from '../services/user.service'
-// import { useSelector, useDispatch } from 'react-redux'
-
-
-=======
 import { useLocation } from "react-router-dom"
->>>>>>> 0de13fd3dc0332a3869e7149b34966cb20ba5db5
 export function LoginSignup() {
     const location = useLocation()
     // const params = useParams()
