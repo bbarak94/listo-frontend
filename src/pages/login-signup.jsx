c4d44f643eb1d38677a92d501b95842e01c13ae8
Guy <guyelizarov@gmail.com>
2022-05-25 13:39:36 +0300

    changes in login&logout cmp

--- src/pages/login-signup.jsx
+++ src/pages/login-signup.jsx
@@ -1,97 +1,16 @@
-import { useState, useEffect } from 'react'
-// import { userService } from '../services/user.service'
-import { useSelector, useDispatch } from 'react-redux'
+// import { useState, useEffect } from 'react'
+// // import { userService } from '../services/user.service'
+// import { useSelector, useDispatch } from 'react-redux'
 
 
 export function LoginSignup() {
 
-    const dispatch = useDispatch()
-    const [credentials, setCredentials] = useState({ username: '', password: '', fullname: '' })
-    const isSignup = useRef(false)
-
-    // const [isSignup, setIsSignup] = useState(false)
-    // const [users, setUsers] = useState([])
-
-    // useEffect(async () => {
-    // const users = await userService.getUsers()
-    //     setUsers(users)
-    // }, [])
-
-    const clearState = () => {
-        setCredentials({ username: '', password: '', fullname: '', imgUrl: '' })
-        setIsSignup(false)
-    }
-
-    const handleChange = ev => {
-        const field = ev.target.name;
-        const value = ev.target.value;
-        setCredentials({ ...credentials, [field]: value });
-    }
-
-    const onLogin = (ev = null) => {
-        if (ev) ev.preventDefault();
-        // if (!credentials.username) return;
-        // props.onLogin(credentials);
-        clearState()
-    }
-
-    const onSignup = (ev = null) => {
-        if (ev) ev.preventDefault();
-        // if (!credentials.username || !credentials.password || !credentials.fullname) return;
-        // props.onSignup(credentials);
-        clearState()
-    }
-
-
-    // const onUploaded = (imgUrl) => {
-    //     setCredentials({ ...credentials, imgUrl });
-    // }
-
-    return (
-<div className="login-page">
-        
-}
+ 
 
 
     return (
         <div className="login-page">
-            {/* <p>
-                <button className="btn-link" onClick={toggleSignup}>{!isSignup ? 'Signup' : 'Login'}</button>
-            </p> */}
-            {!isSignup && <form className="login-form" onSubmit={onLogin}>
 
-                <button>Login!</button>
-            </form>}
-
-            <div className="signup-section">
-                {/* {isSignup && <form className="signup-form" onSubmit={onSignup}>
-                    <input
-                        type="text"
-                        name="fullname"
-                        value={credentials.fullname}
-                        placeholder="Fullname"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        type="text"
-                        name="username"
-                        value={credentials.username}
-                        placeholder="Username"
-                        onChange={handleChange}
-                        required
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        value={credentials.password}
-                        placeholder="Password"
-                        onChange={handleChange}
-                        required
-                    />
-                    {/* /* <ImgUploader onUploaded={onUploaded} /> */}
-                    {/* <button >Signup!</button>
-                </form>
-                    </div>  */}
-            </div>
+        </div>
     )
+}
