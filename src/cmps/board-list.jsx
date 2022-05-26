c106bf3f73224e8b7b6ca6584faa1c71dba4eb78
bbarak94 <bbarak94@gmail.com>
2022-05-26 17:40:36 +0300

    added popups in task details

--- src/cmps/board-list.jsx
+++ src/cmps/board-list.jsx
@@ -1,19 +1,12 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { loadBoards,saveBoard } from '../store/actions/board.action'
+import { loadBoards, saveBoard } from '../store/actions/board.action'
 import { BoardPreview } from './board-preview'
-import { boardService } from '../services/board.service'
-import { AddNewBoard } from './add-new-board'
+import { DynamicPopup } from './dynamic-cmps/dynamic-cmp'
 
 export const BoardList = (props) => {
     const { boards } = useSelector((storeState) => storeState.boardModule)
     const dispatch = useDispatch()
-
-    const onCreateNewBoard = () => {
-        const board = boardService.getEmptyBoard()
-        // console.log('board:', board)
-        dispatch(saveBoard(board))
-    }
 
     useEffect(() => {
         dispatch(loadBoards())
@@ -25,27 +18,7 @@ export const BoardList = (props) => {
             {boards.map((board) => (
                 <BoardPreview board={board} key={board._id} />
             ))}
-            {/* <div
-                className='board-preview flex column align-center justify-center'
-                style={{ backgroundColor: '#091e420a' }}
-                onClick={() => {
-                    console.log('new board created')
-                    onCreateNewBoard()
-                }}
-            >
-                <div>
-                    <h2
-                        style={{
-                            color: '#172b4d',
-                            margin: 'auto',
-                            fontSize: '14px',
-                        }}
-                    >
-                        create new board
-                    </h2>
-                </div>
-            </div> */}
-            <AddNewBoard onCreateNewBoard={onCreateNewBoard}/>
+            <DynamicPopup name={'new-board'} />
         </div>
     )
 }
